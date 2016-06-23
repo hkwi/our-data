d47a052eca18e32223f8244bf9178853734c5c64
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2016-06-24 08:23:47 +0900

    add infection reparse

--- Makefile
+++ Makefile
@@ -3,15 +3,18 @@ all: nursery infection nobi.json
 nobi.csv:
 	PYTHONIOENCODING=utf8 python3 nobi_fetch.py > nobi.csv
 
-nobi.json:
+nobi.json: nobi.csv
 	PYTHONIOENCODING=utf8 python3 nobi.py > nobi.json
 
 .PHONY: nursery
 nursery:
 	python3 nursery_urls.py | wget -x -N -i -
 	python nursery_pdftocsv.py
+	python3 nursery_reparse.py
 
 .PHONY: infection
 infection:
 	python3 infection_urls.py | wget -x -N -i -
 	python infection_pdftocsv.py
+	python3 infection_reparse.py
+
---
+++ infection_reparse.py
@@ -0,0 +1,24 @@
+# coding: UTF-8
+import csv
+import glob
+
+def proc(fn, out):
+	w = csv.writer(open(out, "w"))
+	idx = []
+	subindex = False
+	for l in csv.reader(open(fn)):
+		if l[0] == "疾病名称":
+			for i,r in reversed(list(enumerate(l))):
+				if r.find("～") >= 0:
+					idx = l[:i]
+					break
+			w.writerow(idx)
+		elif l[0].find("報告定点数") >= 0:
+			w.writerow([])
+		elif l[0].find("定点機関から報告されたその他の感染症情報") > 0:
+			break
+		elif idx:
+			w.writerow(l[:len(idx)])
+
+for f in glob.glob("infection/*.pdf.csv"):
+	proc(f, f.replace(".pdf", ".fmt"))
