ac3d93636bec7d1faa4f57c24549bca405c62193
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2016-09-21 11:54:38 +0900

    sync with upstream

--- shinseido_pdftocsv.py
+++ shinseido_pdftocsv.py
@@ -7,7 +7,7 @@ import glob
 fns = glob.glob("www.city.kobe.lg.jp/child/grow/shinseido/img/*sisetuitiran23*.pdf")
 fns = sorted([(os.stat(f).st_mtime, f) for f in fns], reverse=True)
 fn = fns[0][1]
-assert os.path.basename(fn) == "20160426sisetuitiran23.pdf"
+assert os.path.basename(fn) == "280920sisetuitiran23gou.pdf"
 for page in range(2, 11):
 	a = pte.process_page(fn, str(page))
 	x = pte.table_to_list(a, page)
@@ -18,7 +18,7 @@ for page in range(2, 11):
 fns = glob.glob("www.city.kobe.lg.jp/child/grow/shinseido/img/*sisetuitiran1*.pdf")
 fns = sorted([(os.stat(f).st_mtime, f) for f in fns], reverse=True)
 fn = fns[0][1]
-assert os.path.basename(fn) == "280401sisetuitiran1gou.pdf"
+assert os.path.basename(fn) == "280920sisetuitiran1gou.pdf"
 for page in range(2, 6):
 	a = pte.process_page(fn, str(page))
 	x = pte.table_to_list(a, page)
@@ -29,7 +29,7 @@ for page in range(2, 6):
 fns = glob.glob("www.city.kobe.lg.jp/child/grow/shinseido/img/*sisetuitirankourituyoutien.pdf")
 fns = sorted([(os.stat(f).st_mtime, f) for f in fns], reverse=True)
 fn = fns[0][1]
-assert os.path.basename(fn) == "20150930sisetuitirankourituyoutien.pdf"
+assert os.path.basename(fn) == "280920sisetuitirankourituyoutien.pdf"
 a = pte.process_page(fn, "1")
 x = pte.table_to_list(a, 1)
 with open("shinseido/sisetuitirankourituyoutien.csv", "w") as o:
