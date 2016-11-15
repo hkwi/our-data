00a8cc1709fbb5c563a843c38d6d643f68fd82f9
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2016-11-15 17:44:08 +0900

    [nursery] add ninkagai

--- nursery_urls.py
+++ nursery_urls.py
@@ -1,6 +1,6 @@
 import re
 import lxml.html
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 
 base = "http://www.city.kobe.lg.jp/child/grow/shinseido/index02_03.html"
 hrefs = [urljoin(base, x.get("href")) for x in lxml.html.parse(base).xpath("//a")]
@@ -15,3 +15,14 @@ pdfs = [url for url in hrefs if re.match(
 	r"http://www\.city\.kobe\.lg\.jp/child/grow/shinseido/img/(.*)\.pdf", url)]
 for url in pdfs:
 	print(url)
+
+base = "http://www.city.kobe.lg.jp/child/grow/nursery/ninkagai/sisetu.html"
+print(base)
+root = lxml.html.parse(base).getroot()
+root.make_links_absolute()
+for href in root.xpath("//a/@href"):
+	p = urlparse(href)
+	if p.netloc=="www.city.kobe.lg.jp":
+		for ext in (".pdf",".xls",".xlsx"):
+			if p.path.lower().endswith(ext):
+				print(href)
